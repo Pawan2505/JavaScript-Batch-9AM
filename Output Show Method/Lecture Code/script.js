6ff310bbcdcd7202ce1339ab2f862723469fb746
pawan2505 <pawanaktu@gmail.com>
2025-01-23 13:17:43 +0530

    Output show Method

---
+++ Output Show Method/Lecture Code/script.js
@@ -0,0 +1,16 @@
+// console.log(25+9);
+
+// console.log("Hello World")
+
+// document.write("Hey, How are you?")
+
+// alert("Please pay Attension!");
+//
+// document.getElementById('heading-2').innerHTML = "Hello Bhailog!";
+// document.getElementById("heading-2").innerText = `<i>Hello Bhai Log</i>`;
+// document.getElementById("heading-2").innerHTML = `<i>Hello Bhai Log</i>`;
+
+
+// document.getElementsByClassName('dom-class')[0].innerHTML = "Hello Bhailog!";
+
+document.getElementsByClassName('dom-class')[1].innerHTML = "<i>All of you looking ghandsome and beautiful!</i>";
