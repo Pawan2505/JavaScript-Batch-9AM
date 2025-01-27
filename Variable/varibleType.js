afc284e929d7a8dfe94be214255f98f1c0a943bc
pawan2505 <pawanaktu@gmail.com>
2025-01-27 10:07:41 +0530

    Operator:

---
+++ Variable/varibleType.js
@@ -0,0 +1,69 @@
+//  let , const, var  => keyword
+
+// JavaScript has 8 Datatypes
+// String
+// Number
+// Bigint
+// Boolean
+// Undefined
+// Null
+// Symbol
+// Object
+
+// let num = 10;
+// console.log(num);
+
+// console.log(typeof num);
+
+// let name = "Pawan Maurya";
+// console.log(name);
+// console.log(typeof name)
+
+// let isvalue = true;
+// console.log(isvalue);
+// console.log(typeof isvalue);
+
+
+// let x = null;
+
+// console.log(x)
+
+// console.log(typeof x);
+
+// let num1;
+
+// console.log(num1);
+// console.log(typeof num1);
+
+// let brr = []; // empty array
+
+// let arr = [10,20,30,40,50];
+
+// console.log(arr);
+// console.log(typeof arr);
+
+
+
+// let value = 28.78;
+
+// console.log(value);
+// console.log(typeof value);
+
+
+// let num2 = 1234567n;
+
+// console.log(num2);
+// console.log(typeof num2);
+
+
+// let num4 = Symbol(12345);
+
+// console.log(num4.toString());
+// console.log(typeof num4)
+
+
+
+let sejal = {name : "Sejal", age : 123, salary : 100000000};
+
+console.log(sejal);
+console.log(typeof sejal);
