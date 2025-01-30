ce178021d862d9a385dfb9eaaa55ed8ca0c3f214
pawan2505 <pawanaktu@gmail.com>
2025-01-30 10:00:09 +0530

    Conditional Statement, Project, min & max concept

---
+++ Min and Max/script.js
@@ -0,0 +1,18 @@
+
+// let minValue = Math.min(12,85,63,78,96,2,45);
+
+// console.log(minValue);
+
+
+// let maxValue = Math.max(12,85,63,78,96,2,45);
+
+// console.log(maxValue);
+
+
+// let arr = [];
+
+let arr1 = [12,85,63,78,96,2,45];
+
+let minValue = Math.min(...arr1);
+
+console.log(minValue);
