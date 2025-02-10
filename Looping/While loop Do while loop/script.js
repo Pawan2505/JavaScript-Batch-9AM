18b915e4c1fbb649e78091a03b57fa6aa13a1b15
pawan2505 <pawanaktu@gmail.com>
2025-02-10 10:15:57 +0530

    Loop

--- Looping/While loop Do while loop/script.js
+++ Looping/While loop Do while loop/script.js
@@ -37,31 +37,168 @@
 // 3. Power of a number
 // 👉 Compute X^Y (X raised to the power Y) without using pow() function.
 
-let num = 5;
-let n = 2;
+// Method : 1 
 
-let answer = Math.pow(5,2);
+// let num = 2;
+// let x = 3;
 
-console.log(answer);
+// let ans = Math.pow(num,x);
+
+// console.log(ans);
+
+// Method 2 : 
+
+// function power(num, exp){
+
+//     let result = 1;
+
+//     for(let i =1; i <= exp; i++){
+//         result *= num;
+//     }
+
+//     return result;
+// }
+
+// let num = 2;
+// let exp = 3;
+
+// let ans = power(num,exp);
+
+// console.log(ans);
+
 
 // 4. Palindrome number
 // 👉 Check if a number reads the same forward and backward (e.g., 121, 1221).
 
+
+
+// let num = 1121; //  => 1211
+
+// let temp = num;
+
+// let rev = 0;
+
+// while(num > 0){
+//     let digit = num%10;
+
+//     rev = rev*10 + digit;
+
+//     num = Math.floor(num/10);
+
+// }
+
+// if(temp == rev){
+//     console.log("Palindrome number")
+// }else{
+//     console.log("Not Palindrome number")
+// }
+
+
 // 5. Armstrong number
 // 👉 Check if a number is equal to the sum of the cubes of its digits (e.g., 153 = 1³ + 5³ + 3³).
+
+// step 1 : count digit of number
+
+// step 2 : last digit remove => power -> count
+
+// step 3 :  sum - > every digit power
+
+
+// let num =  1634;
+
+// let temp = num;
+
+// let numstr = num.toString().length;
+
+// let sum = 0;
+
+// while(num > 0){
+
+//     let digit = num%10;
+
+//     sum += Math.pow(digit,numstr);
+
+//     num = Math.floor(num/10);
+
+// }
+
+// if(temp == sum){
+//     console.log("Armstrong number")
+// }else{
+//     console.log("Not Armstrong number")
+// }
+
+
+
+
+
+
+
 
 // 6. Finding Fibonacci Series up to N terms
 // 👉 Print the first N terms of the Fibonacci sequence.
 
+// 0,1,1,2,3,5,8,13..
+
+// let n1 = 0;
+// let n2 = 1;
+
+// console.log(n1);
+// console.log(n2);
+
+// let series = 10; 
+
+// for(let i = 2; i < series; i++){
+//     let n3 = n1+n2;
+//     console.log(n3);
+//     n1 = n2;
+//     n2 = n3;
+// }
+
+
 // 7. Strong number
 // 👉 A number where the sum of the factorial of its digits equals the number itself (e.g., 145 = 1! + 4! + 5!).
+
+// function fact(num){
+//     if(num == 0 || num == 1){
+//         return 1;
+//     }
+
+//     return num * fact(num-1);
+// }
+
+// let num = 405855;
+
+// let sum = 0;
+
+// let temp = num;
+
+// while(num > 0){
+//     let digit = num%10;
+//     sum += fact(digit);
+//     num = Math.floor(num/10);
+// }
+
+// console.log(sum)
+
+// if(temp == sum){
+//     console.log("Strong number")
+// }else{
+//     console.log("Not Strong number")
+// }
 
 
 //  8. Perfect number
 // 👉 A number whose sum of divisors (excluding itself) equals the number (e.g., 6 = 1 + 2 + 3).
 
+
+
+
 //  9. Automorphic number
 // 👉 A number whose square ends with the same digits as the number itself (e.g., 5² = 25, 25² = 625).
+
+
+
 
 // 10. Harshad number
 // 👉 A number that is divisible by the sum of its digits (e.g., 18: 1+8 = 9 and 18 % 9 == 0).
