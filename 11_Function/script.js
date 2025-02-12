52d3ef084586d5f3fc26b560e61944e55958a4a9
pawan2505 <pawanaktu@gmail.com>
2025-02-12 10:16:22 +0530

    Function

---
+++ 11_Function/script.js
@@ -0,0 +1,125 @@
+
+// Normal function
+
+
+// function add(x,y){
+//     let result = x*y;
+
+//     return result;
+// }
+
+
+// console.log(add(10,2));
+
+// function add(x,y){
+//     let result = x*y;
+
+//    console.log(result);
+// }
+
+
+// // console.log(add(10,2)); // undefine
+
+// add(10,2);
+
+
+ // anonymous function
+
+//  let result = function(num1,num2){
+//     console.log(num1+num2);
+//  }
+
+
+//  result(10,20);
+
+
+//  result(50,10);
+
+//  result  = "hello"
+
+//  console.log(result)
+
+
+
+//  const result = function(num1,num2){
+//     console.log(num1+num2);
+//  }
+
+
+//  result(10,20);
+
+
+//  result(50,10);
+
+//  result = "hello"
+
+//  console.log(result)
+
+
+// let x = 10;
+
+// console.log(x)
+
+// x = 20;
+
+// console.log(x)
+
+
+
+// let result = function(){
+//     return "Hello World";
+// }
+
+// console.log(result);
+
+
+// result = "hello"
+
+// console.log(result)
+
+
+// let result = function(){
+//     return "Hello World";
+// }
+
+// console.log(result());
+
+
+// result = "hello"
+
+// console.log(result)
+
+
+
+// Arrow function
+
+// syntax
+
+// ()=>{}
+
+// function(num1,num2){
+//     console.log(num1+num2);
+//  }
+
+
+//   const arrow = (a,b) =>{
+
+//         console.log(a+b);
+
+//   }
+
+
+//   arrow(12,36);
+
+//   const arrow = (a,b) =>{
+
+//         console.log(a+b);
+
+//   }
+
+//   arrow(12,36); 
+
+
+
+
+
