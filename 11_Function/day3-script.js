97120956e8028ee9d6cb57633a49174c94d1fb80
pawan2505 <pawanaktu@gmail.com>
2025-02-14 11:52:26 +0530

    Function

---
+++ 11_Function/day3-script.js
@@ -0,0 +1,71 @@
+
+// find factorial 
+
+
+// function fact(num){
+//     //Base Condition
+
+//     if(num == 0 || num == 1){
+//         return 1;
+//     }
+
+//     return num*fact(num-1);
+// }
+
+
+// console.log(fact(5));
+
+
+// sum of natural number
+
+
+// function sumNatural(num){
+//     //Base Condition
+
+//     if(num == 0 || num == 1){
+//         return 1;
+//     }
+
+//     return num+sumNatural(num-1);
+// }
+
+
+// console.log(sumNatural(10));
+
+
+// function sumNatural(num){
+//     //Base Condition
+
+//     if(num == 0 || num == 2){
+//         return 2;
+//     }
+
+//     return num+sumNatural(num-2);
+// }
+
+
+// console.log(sumNatural(10));
+
+
+// Default parameter
+
+
+// function add(x = 90 , y=80){
+//     console.log(x+y);
+// }
+
+// add(10,30); // 40
+// add(20); // 100
+
+// add();
+
+
+// function add(y ="",x = "90"){
+//     console.log(x)
+//     console.log(x+y);
+// }
+
+// add(10,30); // 40
+// add(20); // 100
+
+// add();
