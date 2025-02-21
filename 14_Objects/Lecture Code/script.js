a0dd07c00e10d61e09588e4c750bfbee87ab3053
pawan2505 <pawanaktu@gmail.com>
2025-02-21 10:03:36 +0530

    Object

---
+++ 14_Objects/Lecture Code/script.js
@@ -0,0 +1,93 @@
+
+// without object
+
+// let name = "Pawan";
+// let age = 25
+// let salary = 99
+
+
+//with object
+
+// const person = {
+//     name: "Pawan",
+//     age: 24,
+//     id: 11
+// }
+
+
+// console.log(person)
+
+// console.log(person.name)
+
+// console.log(person.age)
+
+// console.log(person.id)
+
+// console.log(person["name"])
+
+// console.log(person["id"])
+
+
+
+// const person = {
+//   name: "Pawan",
+//   age: 24,
+//     id: 11,
+//     teaching: function () {
+//         console.log("Teaching JavaScript")
+//     }
+// };
+
+// console.log(person)
+
+// person.teaching();
+
+
+
+// const person = {
+//   name: "Pawan",
+//   age: 24,
+//   id: 11,
+
+//   teaching: function () {
+//       console.log(this) // this keyword refer current object
+//      console.log(`I am ${this.name}. My age is ${this.age} and my id is ${this.id}`)
+//   }
+// };
+
+// // console.log(person);
+
+// person.teaching();
+
+// person["name"] = "Ram";  // update key value
+// person.name = "Radha"
+
+// person.salary = 999
+// console.log(person);
+
+// delete person.salary;
+
+// console.log(person);
+
+// Object with array
+
+// let student = {
+//     name: "dhruv",
+//     subject:["OS","CPP","DSA","Networking","Web development"]
+// }
+
+// console.log(student)
+
+
+// console.log(student.subject[2]);
+
+// delete student.subject[2];
+
+// console.log(student)
+
+// student.subject[2] = "Kya haal hai";
+// console.log(student);
+// student.subject[5] = "Fine";
+// console.log(student);
+
+
