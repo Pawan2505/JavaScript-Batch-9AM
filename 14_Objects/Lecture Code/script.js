5b296caed6c6b2df334ea68df479a6c9687aaeab
pawan2505 <pawanaktu@gmail.com>
2025-02-21 10:08:50 +0530

    JS Object

--- 14_Objects/Lecture Code/script.js
+++ 14_Objects/Lecture Code/script.js
@@ -1,93 +1,116 @@
+// Example 1: Without using Object
 
-// without object
+// Here, we define individual variables to store information
+let name = "Pawan"; // Store name
+let age = 25; // Store age
+let salary = 99; // Store salary
 
-// let name = "Pawan";
-// let age = 25
-// let salary = 99
+// Displaying the values
+console.log(name); // Logs the value of the name
+console.log(age); // Logs the value of the age
+console.log(salary); // Logs the value of the salary
 
+// Example 2: Using Object to store multiple pieces of information
 
-//with object
+// Here, we use an object to group related properties together
+const person = {
+  name: "Pawan", // Property: name
+  age: 24, // Property: age
+  id: 11, // Property: id
+};
 
-// const person = {
-//     name: "Pawan",
-//     age: 24,
-//     id: 11
-// }
+// Displaying the object
+console.log(person); // Logs the entire object
 
+// Accessing object properties using dot notation
+console.log(person.name); // Logs the value of the 'name' property
+console.log(person.age); // Logs the value of the 'age' property
+console.log(person.id); // Logs the value of the 'id' property
 
-// console.log(person)
+// Accessing object properties using bracket notation
+console.log(person["name"]); // Logs the value of the 'name' property
+console.log(person["id"]); // Logs the value of the 'id' property
 
-// console.log(person.name)
+// Example 3: Using function within an object
 
-// console.log(person.age)
+// In this example, we define a method (function) inside the object
+const personWithFunction = {
+  name: "Pawan",
+  age: 24,
+  id: 11,
 
-// console.log(person.id)
+  // A method that logs a message
+  teaching: function () {
+    console.log("Teaching JavaScript");
+  },
+};
 
-// console.log(person["name"])
+// Calling the method within the object
+personWithFunction.teaching(); // Logs: Teaching JavaScript
 
-// console.log(person["id"])
+// Example 4: Using 'this' keyword inside an object method
 
+// 'this' refers to the current object in the context of a method
+const personWithThis = {
+  name: "Pawan",
+  age: 24,
+  id: 11,
 
+  // A method that uses the 'this' keyword
+  teaching: function () {
+    console.log(this); // Logs the current object (this refers to the object)
+    console.log(
+      `I am ${this.name}. My age is ${this.age} and my id is ${this.id}`
+    );
+    // Logs: I am Pawan. My age is 24 and my id is 11
+  },
+};
 
-// const person = {
-//   name: "Pawan",
-//   age: 24,
-//     id: 11,
-//     teaching: function () {
-//         console.log("Teaching JavaScript")
-//     }
-// };
+// Calling the method
+personWithThis.teaching();
 
-// console.log(person)
+// Example 5: Updating properties and deleting them in an object
 
-// person.teaching();
+// Updating the value of a property using dot notation
+personWithThis.name = "Radha";
 
+// Adding a new property 'salary'
+personWithThis.salary = 999;
 
+// Displaying the updated object
+console.log(personWithThis); // Logs updated object with new name and salary
 
-// const person = {
-//   name: "Pawan",
-//   age: 24,
-//   id: 11,
+// Deleting the 'salary' property from the object
+delete personWithThis.salary;
 
-//   teaching: function () {
-//       console.log(this) // this keyword refer current object
-//      console.log(`I am ${this.name}. My age is ${this.age} and my id is ${this.id}`)
-//   }
-// };
+// Displaying the object after deletion
+console.log(personWithThis); // Logs object after 'salary' property has been deleted
 
-// // console.log(person);
+// Example 6: Object containing an array
 
-// person.teaching();
+// Here, we store an array inside the object as a property
+let student = {
+  name: "Dhruv", // Student's name
+  subject: ["OS", "CPP", "DSA", "Networking", "Web Development"], // List of subjects
+};
 
-// person["name"] = "Ram";  // update key value
-// person.name = "Radha"
+// Displaying the student object
+console.log(student);
 
-// person.salary = 999
-// console.log(person);
+// Accessing an element of the array inside the object
+console.log(student.subject[2]); // Logs: DSA (the third subject in the array)
 
-// delete person.salary;
+// Deleting an element from the array inside the object
+delete student.subject[2];
 
-// console.log(person);
+// Displaying the object after deletion (notice that index 2 is now empty)
+console.log(student);
 
-// Object with array
+// Updating an element in the array inside the object
+student.subject[2] = "Kya haal hai"; // Replaces the deleted element with a new value
 
-// let student = {
-//     name: "dhruv",
-//     subject:["OS","CPP","DSA","Networking","Web development"]
-// }
+// Adding a new subject to the array (at index 5)
+student.subject[5] = "Fine";
 
-// console.log(student)
-
-
-// console.log(student.subject[2]);
-
-// delete student.subject[2];
-
-// console.log(student)
-
-// student.subject[2] = "Kya haal hai";
-// console.log(student);
-// student.subject[5] = "Fine";
-// console.log(student);
-
-
+// Displaying the object with updated subjects
+console.log(student);
