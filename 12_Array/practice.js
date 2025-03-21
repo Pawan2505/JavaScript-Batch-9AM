8b611723109af6bd3e8e201fb8cd44fc37ef1307
pawan2505 <pawanaktu@gmail.com>
2025-03-21 10:17:31 +0530

    practice

---
+++ 12_Array/practice.js
@@ -0,0 +1,26 @@
+// const arr = [2, 3,4];
+
+// const result = arr.reduce((acc, item) => acc * item, 1);
+// console.log(result);
+
+// const arr = [2, 3,4];
+
+// const result = arr.reduce((acc, item) => (
+//  acc * item
+// )
+// );
+// console.log(result);
+
+// const arr = [2, 3, 4];
+
+// arr.forEach((item,index) => {
+//     console.log(item)
+// })
+
+const arr = [2, 3, 4];
+
+arr.map((item) => {
+  console.log(item);
+});
+
+// console.log(brr);
